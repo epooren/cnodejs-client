cdd2d175a8a4cfac7a3087924c73c7d4430a14e8
epooren <mr_zhangjiayong@163.com>
2016-03-27 08:45:03 +0800

    正常开发UI  开始接入store

--- src/components/Navigation/Route.js
+++ src/components/Navigation/Route.js
@@ -7,8 +7,7 @@ const {FloatFromRight} = Navigator.SceneConfigs;
 
 class Route {
   constructor({Component, props, scene = FloatFromRight} = args) {
-    // TODO 验证Component参数必须存在 且必须是Component
-    if ((Object.getPrototypeOf(Component) !== React.Component)) {
+    if (getAncestors(Component).indexOf(React.Component) === -1) {
       throw new Error('Component不是React.Component子类');
     }
 
@@ -23,5 +22,23 @@ class Route {
 Route.isInstance = function (obj) {
   return obj instanceof Route;
 };
+
+
+
+
+getAncestors.result = [];
+
+function getAncestors(c) {
+    let parent = Object.getPrototypeOf(c);
+
+    if (parent) {
+        getAncestors.result.push(parent);
+        return getAncestors(parent);
+    } else {
+        let result = getAncestors.result;
+        getAncestors.result = [];
+        return result;
+    }
+}
 
 module.exports = Route;
